ca35000e46f690a9afd3fae674138158d46e477d
Ramzi-dev611 <ramzi.latrous@insat.u-carthage.tn>
2022-03-16 01:32:46 +0100

    simplified home page , navabr and footer plus a routing system

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Routing } from './app.routing';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    Routing
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,8 +1,9 @@
 import { RouterModule, Routes } from "@angular/router";
+import { HomepageComponent } from "./homepage/homepage.component";
 
 
 const routes: Routes = [
-    { path: '', redirectTo: '', pathMatch: 'full' },
-    // { path: '', component: null}
+    // { path: '', redirectTo: '', pathMatch: 'full' },
+    { path: '', component: HomepageComponent}
 ]
 export const Routing = RouterModule.forRoot(routes)
