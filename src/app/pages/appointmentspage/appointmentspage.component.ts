3a923e526d08ad7937a965c7d49a58b79ed3dd7e
wissembendaly <wissembendaly@insat.u-carthage.tn>
2022-03-16 21:40:35 +0100

    appointmentpage modification

--- src/app/pages/appointmentspage/appointmentspage.component.ts
+++ src/app/pages/appointmentspage/appointmentspage.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { Appointment } from 'src/app/models/appointment.model';
+
+import { User } from 'src/app/models/user.model';
+import { AppointmentService } from 'src/app/services/appointment.service';
+
 
 @Component({
   selector: 'app-appointmentspage',
@@ -6,10 +12,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./appointmentspage.component.css']
 })
 export class AppointmentspageComponent implements OnInit {
-
-  constructor() { }
+  public currentUser: User = new User();
+  public appointments: Appointment[] = [];
+  
+  constructor(private readonly toastr: ToastrService,private readonly appointmentService: AppointmentService) {}
 
   ngOnInit(): void {
+    this.appointmentService.getAppointments().subscribe({
+      'next': data => {
+        this.appointments=data;
+      }
+    });
+
+  }
+
+  public getPass(){
+
+        this.toastr.success('We sent you your pass. Please check your Email')
   }
 
 }
--- src/app/services/appointment.service.ts
+++ src/app/services/appointment.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Appointment } from '../models/appointment.model';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppointmentService {
+  constructor(private readonly http: HttpClient) { }
 
-  constructor() { }
+  public getAppointments(): Observable<Appointment[]>{
+    const userId = localStorage.getItem('id');
+    return this.http.get<Appointment[]>(`http://localhost:5000/appointments?userId=${id}`);
+  }
+
 }
